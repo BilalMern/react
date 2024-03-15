b1c5586309782dc325b510ce7aa1b429660fc489
BilalMern <bilal.mern@gmail.com>
2024-03-16 00:52:13 +0500

    Functional composition

--- 3 Laying the foundation/app.js
+++ 3 Laying the foundation/app.js
@@ -1,11 +1,39 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-//! Creating h1 tag using jsx. 
-//! jsx is not html in js jsx is a html like syntax.
 
-let jsxHeading=<h1>This is h1 tag in jsx!</h1> //! this will also give object and this code is not pure js code and not browser readable parcel is doing the job to make it browser readable thats why the code is working perfectly fine. here parcel tranpiled the code before it reaches to js engine, transpiled means conversion. parcel is not tranpiled the code by itself it gives the responsibility of transpilation to a package which is known as babel. jsx is behind the scenes work as React.createElement
+//! Jsx:
+//* Creating h1 tag using jsx. 
+//? jsx is not html in js jsx is a html like syntax.
 
+// let jsxHeading=<h1>This is h1 tag in jsx!</h1> //* this will also give object and this code is not pure js code and not browser readable parcel is doing the job to make it browser readable thats why the code is working perfectly fine. here parcel tranpiled the code before it reaches to js engine, transpiled means conversion. parcel is not tranpiled the code by itself it gives the responsibility of transpilation to a package which is known as babel. jsx is behind the scenes work as React.createElement
+
+// let root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(jsxHeading)
+
+//! Function Components in React:
+//? Function component is a normal js function which returns some peace of JSX some JSX element OR A function which returns a react element. a name of function must start with capital letter.
+// let FunctionHeading = ()=>{
+//     return <h1>This is function component</h1>
+// }
+//OR (both ways are true.)
+// let FunctionHeading =()=>(
+//     <h1 className="header">This is header</h1>
+// )
+// let root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<FunctionHeading />) //!This is the syntax to render a functional component.
+
+//! calling a functional component inside functional component is known as component composition.
+
+let FunctionComp =()=>(
+     <h3>rendering a component inside component</h3>
+)
+
+let MainFunc =()=>(
+    <div>
+   <h1>This is main function</h1>
+    <FunctionComp/>
+    </div>
+)
 let root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(jsxHeading)
-
+root.render(<MainFunc/>)
