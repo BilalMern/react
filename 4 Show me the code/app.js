07cafe0bbe51eee35c0e80084c58da5e5896198a
BilalMern <bilal.mern@gmail.com>
2024-03-17 02:01:14 +0500

    Brain Storming and planning

--- 4 Show me the code/app.js
+++ 4 Show me the code/app.js
@@ -1,6 +1,22 @@
+/** 
+ *Header
+ *  -Logo
+ *  -Nav Items
+ *Body
+ *  -Search
+ *  -RestaurantContainer
+ *    -RestaurantCard
+ *Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+ 
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 let AppLayout =()=>{
-    
+
 }
