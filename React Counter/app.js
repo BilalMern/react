6e8846760d6c932b16f6ffa654b76789db2218b0
BilalMern <bilal.mern@gmail.com>
2024-07-22 23:42:02 +0500

    Changes

--- React Counter/app.js
+++ React Counter/app.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
-let num = 0;
-const App =()=>{
-    const [numAdd,setNumAdd]=useState(num)
-return(
-    <button onClick={()=>{
-        setNumAdd(numAdd+1)
-    }}>Click to Add {numAdd}</button>
-)
-}
-let root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App/>)
+// let num = 0;
+// const App =()=>{
+//     const [numAdd,setNumAdd]=useState(num)
+// return(
+//     <button onClick={()=>{
+//         setNumAdd(numAdd+1)
+//     }}>Click to Add {numAdd}</button>
+// )
+// }
+// let root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<App/>)
+
+let App = () => {
+  let num = 0;
+  const [addNum, setAddNum] = useState(num);
+  return (
+    <button
+      onClick={() => {
+        setAddNum(addNum + 1);
+      }}
+    >
+      Click to add {addNum}
+    </button>
+  );
+};
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
