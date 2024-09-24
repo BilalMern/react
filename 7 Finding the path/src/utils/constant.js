5055b6390491d9224e5ce176ab5ad5edb1e6048d
BilalMern <bilal.mern@gmail.com>
2024-09-24 12:09:16 -0700

    chapter 6 complete

---
+++ 7 Finding the path/src/utils/constant.js
@@ -0,0 +1,4 @@
+//! In this file we put all the constants means all the hardcoded things.
+
+const LOGO_URL= "https://png.pngtree.com/png-clipart/20200720/original/pngtree-abstract-logo-for-cafe-or-restaurant-graphic-food-icon-symbol-for-png-image_4316818.jpg" //! Its a good practice to name a constant in capital letters and in snake case.
+export default LOGO_URL 
