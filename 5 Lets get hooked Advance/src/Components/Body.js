6aa057c0a2ca77c2842e0fff82e7c7286b4347a7
BilalMern <bilal.mern@gmail.com>
2024-10-07 20:22:41 -0700

    Diff algo, reconciliation, react fibre

--- 5 Lets get hooked Advance/src/Components/Body.js
+++ 5 Lets get hooked Advance/src/Components/Body.js
@@ -1,23 +1,23 @@
-
 import resList from "../utils/mockData";
 import RestaurantCard from './RestaurantCard';
 import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurant,setListOfRestaurant]= useState(resList)
     return (
       <div className="body">
         <div className="search-center">
         <div className="top_rated">
           <button className="filter_btn" onClick = {()=>{
          const filteredList = resList.filter((fil)=>fil.info.avgRating > 4.5)
-        console.log(filteredList)
+       setListOfRestaurant(filteredList)
           }}>Top Rated Restaurants</button>
         </div>
         </div>
        
         <div className="res-container">
           {
-            resList.map((restaurant)=>{
+            listOfRestaurant.map((restaurant)=>{
               return (
                 
                 <RestaurantCard key={restaurant.info.id}  resData = {restaurant} />
