317873de30301c2c828d174a23d71dc90afb70a1
BilalMern <bilal.mern@gmail.com>
2024-10-13 00:34:38 -0700

    Loading

--- 6 Exploring the world Advance/src/Components/Body.js
+++ 6 Exploring the world Advance/src/Components/Body.js
@@ -11,15 +11,22 @@ const Body = () => {
   const fetchData = async () => {
   const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
   const json = await data.json();
+  // Optional chaining.
   setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   };
+
+  if(listOfRestaurant.length===0){
+    return (
+    <h1>Loading...</h1>
+    )
+  }
 
   return (
       <div className="body">
         <div className="search-center">
         <div className="top_rated">
           <button className="filter_btn" onClick = {()=>{
-         const filteredList = resList.filter((fil)=>fil.info.avgRating > 4.5)
+         const filteredList = listOfRestaurant.filter((fil)=>fil.info.avgRating > 4.4)
        setListOfRestaurant(filteredList)
           }}>Top Rated Restaurants</button>
         </div>
