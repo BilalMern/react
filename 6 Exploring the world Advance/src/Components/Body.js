a3761ef537c9c952e32de06b1ec728472b296e33
BilalMern <bilal.mern@gmail.com>
2024-10-13 21:53:30 -0700

    case insensitive

--- 6 Exploring the world Advance/src/Components/Body.js
+++ 6 Exploring the world Advance/src/Components/Body.js
@@ -1,54 +1,70 @@
-
-import RestaurantCard from './RestaurantCard';
+import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import Shimmer from './Shimmer';
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant,setListOfRestaurant]= useState([])
-  useEffect(()=>{
-   fetchData();
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    fetchData();
   }, []);
 
   const fetchData = async () => {
-  const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-  const json = await data.json();
-  // Optional chaining.
-  setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    
+    // Optional chaining.
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
- return listOfRestaurant.length===0?(<Shimmer/>):
-   
-  
-   (
-      <div className="body">
-        <div className="search-center">
-        <div className="top_rated">
-          <button className="filter_btn" onClick = {()=>{
-         const filteredList = listOfRestaurant.filter((fil)=>fil.info.avgRating > 4.4)
-       setListOfRestaurant(filteredList)
-          }}>Top Rated Restaurants</button>
-        </div>
-        </div>
-       
-        <div className="res-container">
-          {
-            listOfRestaurant.map((restaurant)=>{
-              return (
-                
-                <RestaurantCard key={restaurant.info.id}  resData = {restaurant} />
-              )
-            })
-          }
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="search-center">
+        <div className="search">
+        
+          <input
+            type="text"
+            className="input-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value)
+            }}
+          />
           
+          <button onClick={()=>{
+          const filteredRestaurant = listOfRestaurant.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+         setListOfRestaurant(filteredRestaurant)
+         }}>Search</button>
+        </div>
+        <div className="top_rated">
+          <button
+            className="filter_btn"
+            onClick={() => {
+              const filteredList = listOfRestaurant.filter(
+                (fil) => fil.info.avgRating > 4.4
+              );
+              setListOfRestaurant(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
         </div>
       </div>
-    );
-  
-  };
-  export default Body;
 
-
-
-
-
-
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Body;
