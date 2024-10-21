98ec6ee9304bb095cbf23fcfb0efb9ae0676c3b9
BilalMern <bilal.mern@gmail.com>
2024-10-20 23:10:51 -0700

    config routes

--- 7 Finding the path Advance/src/app.js
+++ 7 Finding the path Advance/src/app.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-
-   
-
+import About from "./Components/About";
+import About from "./Components/About";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 const AppLayout = () => {
-  
   return (
     <div className="app">
       <Header />
-      
+
       <Body />
     </div>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <AppLayout/>
+  },
+  {
+    path: "/about",
+    element: <About/>
+  }
+])
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
+
